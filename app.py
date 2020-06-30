7b3fd346bfef4858f1dbcb6a5eeafb8b55f6075c
Akash Rana <akashrana20022000@gmail.com>
2020-06-30 16:11:05 +0530

    working drop and go.scatter

--- app.py
+++ app.py
@@ -8,110 +8,158 @@ import src
 import plotly.express as px
 import json
 import dash_table
+import os
 
 time_series  = src.case_time_series()
 state_total,last_update = src.states_wise()
 state_daily = src.daily_state()
 state_cumu  = src.state_cumu()
+state_codes_zip = zip(state_total.State.unique(), state_total.State_code.unique())
+state_codes = dict(zip(state_total.State_code.unique(),state_total.State.unique()))
+
 
 external_stylesheets = []
 tickFont = {'size':12, 'color':"rgb(30,30,30)", \
             'family':"Courier New, monospace"}
 
-
-
-app = dash.Dash(__name__,external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 
 app.title =  'Covid-19 Tracker'
 
 app._favicon = 'img/favicon.png'
 
-app.layout = html.Div( children  = [
-    html.H1(children = "Covid-19 Tracker", id = 'h1id'),
 
-    html.P(id = 'last',children = "LAST UPDATED: "+str(last_update)),
-    html.Div([
-                dash_table.DataTable(
-                    id='table',
-                    columns=[{"name":i,"id":i} for i in state_total.columns[:5]],
-                    data=state_total.to_dict('records'),
-                    style_cell={'textAlign':'left'},
-                    style_table={'height':'40%','width':'95%','margin-left':'40px','margin-up':'10px'},
-                    style_data_conditional=[{
-                            'if':{'row_index':'odd'},
-                            'backgroundColor':'rgb(248, 248, 248)'
-                    }],
-                    style_header={
-                            'backgroundColor': 'rgb(230, 230, 230)',
-                            'fontWeight': 'bold'
-                    }
-                )]),
-    html.Div(id = 'state-graph',children = [
+                                    #'''Scatter Plot'''#
+state_scatter = px.scatter(state_total[1:], x='Confirmed', y='Deaths', size_max=150,
+                        size='Confirmed', color='State',log_x=True)
+state_scatter.update_layout(plot_bgcolor='#003f5c',
+                        xaxis=dict(showline=False,showgrid=False,zeroline=False),
+                        yaxis=dict(showline=False, showgrid=False,zeroline=False),
+                            title={
+                            'text': 'Relation b/w Deaths and Confirmed Cases',
+                            'y': 0.95,
+                            'x': 0.4,
+                            'xanchor': 'center',
+                            'yanchor': 'top'}
+                        )
+
+
+app.layout = html.Div( children  = [
+    html.H1(children = "Covid-19 Tracker", id = 'h1id',),
+
+    html.P(id = 'last_update',children = "LAST UPDATED: "+str(last_update)),
+    
+    html.Div(children = [
         dcc.Dropdown(
             id = 'dropdown',
-            options=[
-                {'label': i, 'value': j} for i,j in zip(state_total.State.unique(),state_total.State_code.unique())
-            ],
+            options=[{'label': i, 'value': j} for i,j in state_codes_zip],
             value='TT'
         ),
-        dcc.Graph(
-            id = 'time-series-total',
-            className = 'graph',
-                ),
-        dcc.Graph(
-            id = 'time-series-daily',
-            className = 'graph',
-        )
-    ]),
-    html.Div(id = 'state-scatter',children =[
-        dcc.Slider(
-            id = 'mortality-slider',
-            min = state_cumu['Date'].dt.month.unique().min(),
-            max = state_cumu['Date'].dt.month.unique().max(),
-            step = 1,
-            #markers  = {i: str(i) for i in state_cumu['Date'].dt.month_name().unique()}
 
-            #marks  ={str(j):{ 'Label': str(i)} for i,j in zip(state_cumu['Date'].dt.month_name().unique(),state_cumu['Date'].dt.month.unique())}
+    ]),      
+      
+
+    html.Div(children=[
+
+        dcc.Graph(
+            id='time_series_total',
+            className='graph',
+            config={'displayModeBar': False}
         ),
 
-
         dcc.Graph(
-            className = 'graph',
-            figure = px.scatter(state_total[1:],x = 'Deaths', y ='Confirmed',size_max=150,title='yyas',size='Deaths',color='State',log_x=True),        
-            ),
+            id='time_series_daily',
+            className='graph',
+            config={'displayModeBar': False}
+        )
         
     ]),
-])
+
+    html.Div(children=[
+        dcc.Graph(
+            id='state_scatter_graph',
+            className='graph',
+            figure=state_scatter,
+            config={'displayModeBar': False}
+        ),
+    ]),    
+])   
+
 
 
 @app.callback(
-    [Output('time-series-total','figure'),
-    Output('time-series-daily','figure')],
-    [Input('dropdown','value')]
-)
+              [Output('time_series_total', 'figure'),
+               Output('time_series_daily', 'figure')],
+
+              [Input('dropdown', 'value')])
 
 def update_graph(drop_value):
     
+    figure_total = go.Figure()
+    figure_daily=go.Figure()
     if drop_value == 'TT':
-        figure = px.area(time_series,x = 'Date', y ='Total Confirmed',title='Time Series Total')
-        figure.add_scatter( x = time_series['Date'],y=time_series['Total Deceased'],name='Deceased',fill='tozeroy')
-        figure.add_scatter( x = time_series['Date'],y=time_series['Total Recovered'],name='Recovered',fill='tonexty')
-        daily_fig = px.area(time_series,x = 'Date', y ='Daily Confirmed',title='Daily Time Series')   
-        daily_fig.add_scatter( x = time_series['Date'],y=time_series['Daily Deceased'],name='Deceased',fill='tozeroy')
-        daily_fig.add_scatter( x = time_series['Date'],y=time_series['Daily Recovered'],name='Recovered',fill='tonexty')
 
-    else :
-        figure = px.area(state_cumu[[state_cumu['Status']=='Confirmed']],x = 'Date',y = drop_value,title='Daily Time Series')
-        figure.add_scatter(x =state_cumu[[state_cumu['Status']=='Confirmed']]['Date'],y = state_cumu[drop_value],name='Deceased',fill='tozeroy')
-        figure.add_scatter(x = state_cumu[[state_cumu['Status']=='Confirmed']]['Date'],y = state_cumu[drop_value],name='Recovered',fill='tonexty')
-        daily_fig = px.area(state_daily[state_daily['Status']=='Confirmed'],x = 'Date',y = drop_value,title='Daily Time Series')
-        daily_fig.add_scatter(x =state_daily[state_daily['Status']=='Deceased']['Date'],y = state_daily[state_daily['Status']=='Deceased'][drop_value],name='Deceased',fill='tozeroy')
-        daily_fig.add_scatter(x = state_daily[state_daily['Status']=='Recovered']['Date'],y = state_daily[state_daily['Status']=='Recovered'][drop_value],name='Recovered',fill='tonexty')
+        figure_total.add_trace(go.Scatter(x=time_series['Date'],y=time_series['Total Confirmed'],mode='lines',name='Total Confirmed',text="Confirmed"))
+        figure_total.add_trace(go.Scatter(x=time_series['Date'], y=time_series['Total Recovered'], mode='lines', name='Total Recovered',line_color='#00ff00'))
+        figure_total.add_trace(go.Scatter(x=time_series['Date'], y=time_series['Total Deceased'], mode='lines', name='Total Deceased',line_color='#ff0000'))
+        
+        figure_daily.add_trace(go.Scatter(
+               x = time_series['Date'], y = time_series['Daily Confirmed'], mode = 'lines', name = 'Daily Confirmed'))
+        figure_daily.add_trace(go.Scatter(
+               x = time_series['Date'], y = time_series['Daily Recovered'], mode = 'lines', name = 'Daily Recovered', line_color = '#00ff00'))
+        figure_daily.add_trace(go.Scatter(
+               x = time_series['Date'], y = time_series['Daily Deceased'], mode = 'lines', name = 'Daily Deceased', line_color = '#ff0000'))
 
-    figure.update_layout(transition_duration=500)
-    daily_fig.update_layout(transition_duration=500)
+    else:
 
-    return figure,daily_fig
+        confirmed_cumu = state_cumu[state_cumu['Status'] == 'Confirmed']
+        deceased_cumu = state_cumu[state_cumu['Status'] == 'Deceased']
+        recovered_cumu = state_cumu[state_cumu['Status'] == 'Recovered']
+
+        figure_total.add_trace(go.Scatter(
+                x=confirmed_cumu['Date'], y=confirmed_cumu[drop_value], mode='lines', name='Total Confirmed'))
+        figure_total.add_trace(go.Scatter(
+                x=recovered_cumu['Date'], y=recovered_cumu[drop_value], mode='lines', name='Total Recovered', line_color='#00ff00'))
+        figure_total.add_trace(go.Scatter(
+                x=deceased_cumu['Date'], y=deceased_cumu[drop_value], mode='lines', name='Total Deceased', line_color='#ff0000'))
+
+        confirmed_daily=state_daily[state_daily['Status']== 'Confirmed']
+        recovered_daily=state_daily[state_daily['Status']== 'Recovered']
+        deceased_daily=state_daily[state_daily['Status']== 'Deceased']
+
+        figure_daily.add_trace(go.Scatter(
+                x = confirmed_daily['Date'], y = confirmed_daily[drop_value], mode = 'lines', name = 'Daily Confirmed'))
+        figure_daily.add_trace(go.Scatter(
+                x = recovered_daily['Date'], y = recovered_daily[drop_value], mode = 'lines', name = 'Daily Recovered', line_color = '#00ff00'))
+        figure_daily.add_trace(go.Scatter(
+                x = deceased_daily['Date'], y = deceased_daily[drop_value], mode = 'lines', name = 'Daily Deceased', line_color = '#ff0000'))
+
+    figure_total.update_layout(plot_bgcolor="#f5f5f5",
+                                   xaxis=dict(showline=False, showgrid=False),
+                                   yaxis=dict(showline=False, showgrid=False),
+                                   title={'text': 'Total Cases in %s' % (
+                                       state_codes['%s' % drop_value]), 'xanchor': 'center', 'x': 0.5},
+                                   xaxis_title="Date",
+                                   yaxis_title="Cases",
+                                   xaxis_range=['2020-04-01',
+                                                max(time_series['Date'])],
+                                   showlegend=False,
+                                   )
+        
+
+    figure_daily.update_layout(plot_bgcolor='#f5f5f5',
+                                   xaxis=dict(showline=False, showgrid=False),
+                                   yaxis=dict(showline=False, showgrid=False),
+                                   title={'text': 'Daily Cases in %s' % (
+                                       state_codes['%s' % drop_value]), 'xanchor': 'center', 'x': 0.5},
+                                   xaxis_title="Date",
+                                   yaxis_title="Cases",
+                                   xaxis_range=['2020-04-01',
+                                                max(time_series['Date'])],
+                                   showlegend=False,)
+
+    return figure_total, figure_daily 
 
 server = app.server
 
